674843c7ca63438476d4357072102cdc0ba37b18
kel7lz <klazoalt71@gmail.com>
2026-05-13 09:48:33 +0800

    Delete mercato directory

--- mercato/customer/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ============================================================
-// customer/logout.php — Logout handler
-// ============================================================
-require_once __DIR__ . '/../includes/functions.php';
-
-auth_handle_logout();
--- mercato/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Show all folders in htdocs
-$folders = array_filter(glob('*'), 'is_dir');
-echo "<h1>Folders in htdocs:</h1>";
-echo "<ul>";
-foreach ($folders as $folder) {
-    echo "<li><a href='http://localhost/$folder/'>$folder</a></li>";
-}
-echo "</ul>";
